6bbab81b729e39a3533e2c6a95ed079cbb8de3be
Tunafan <124045808+Tunafan@users.noreply.github.com>
2023-06-07 23:04:57 +0200

    fetch and show donde (y)

--- environment-01/script.js
+++ environment-01/script.js
@@ -1,1 +1,31 @@
 "use strict";
+window.addEventListener("load", initApp)
+
+async function initApp() {
+    const students = await getStudents("./students.json");
+
+    students.forEach(showStudent);
+}
+async function getStudents(url) {
+    const response = await fetch(url)
+    const data = await response.json();
+    return data;
+}
+
+async function showStudent(student){
+    console.log(student);
+
+    document.querySelector("#students-container").insertAdjacentHTML(
+        "beforeend",
+        /*html*/ `
+        <article>
+            <h2>${student.name}</h2>
+            <article id="student-container">
+            <p>Student number: ${student.id}</p>
+            <p>e-mail adress: ${student.mail}</p>
+            <p>Enrolled in faculty: ${student.enrolled}</p>
+            <p>Semesters studied: ${student.semester}</p>
+            </article>
+            </article> `
+    )
+}
